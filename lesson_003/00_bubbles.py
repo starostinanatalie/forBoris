074a4e50c8d0d302f729d2b1682dcf933a228252
starostinanatalie <starostina.natalie@gmail.com>
2024-07-30 20:22:17 +0300

    first commit

---
+++ lesson_003/00_bubbles.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import random
+
+import simple_draw as sd
+
+sd.resolution = (1200, 600)
+
+# Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
+#TODO here's your code
+
+# Написать функцию рисования пузырька, принммающую 3 (или более) параметра: точка рисования, шаг и цвет
+sd.sleep(3)
+sd.clear_screen()
+#TODO here's your code
+
+# Нарисовать 10 пузырьков в ряд
+sd.sleep(3)
+sd.clear_screen()
+#TODO here's your code
+
+# Нарисовать три ряда по 10 пузырьков
+sd.sleep(3)
+sd.clear_screen()
+#TODO here's your code
+
+# Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
+sd.sleep(3)
+sd.clear_screen()
+#TODO here's your code
+
+sd.pause()
+
---
+++ lesson_003/07_wall.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# (цикл for)
+import simple_draw
+
+# Нарисовать стену из кирпичей. Размер кирпича - 100х50
+# Использовать вложенные циклы for
+sd.sleep(3)
+sd.clear_screen()
+
+#TODO here's your code
+
+
+
+simple_draw.pause()
+
